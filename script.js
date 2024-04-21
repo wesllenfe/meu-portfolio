579f04d1233c260b89913d36b6374d6160a6a3c2
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-04-21 18:39:19 -0300

    feat: Added transition to hamburger menu

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
 
-document.getElementById('logo').addEventListener('click', function() {
-  document.getElementById('profile').scrollIntoView({ behavior: 'smooth' });
-});
+  menu.classList.toggle("open");
+  icon.classList.toggle("open"); 
+}
